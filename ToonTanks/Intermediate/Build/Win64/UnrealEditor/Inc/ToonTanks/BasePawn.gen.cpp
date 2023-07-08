27f1eef5f7694acf107453764173bb3d522af0d5
Dimitris Chios <chiosdimit@gmail.com>
2023-07-08 20:41:14 +0300

    initial commit

---
+++ ToonTanks/Intermediate/Build/Win64/UnrealEditor/Inc/ToonTanks/BasePawn.gen.cpp
@@ -0,0 +1,87 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "ToonTanks/BasePawn.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeBasePawn() {}
+// Cross Module References
+	TOONTANKS_API UClass* Z_Construct_UClass_ABasePawn_NoRegister();
+	TOONTANKS_API UClass* Z_Construct_UClass_ABasePawn();
+	ENGINE_API UClass* Z_Construct_UClass_APawn();
+	UPackage* Z_Construct_UPackage__Script_ToonTanks();
+// End Cross Module References
+	void ABasePawn::StaticRegisterNativesABasePawn()
+	{
+	}
+	IMPLEMENT_CLASS_NO_AUTO_REGISTRATION(ABasePawn);
+	UClass* Z_Construct_UClass_ABasePawn_NoRegister()
+	{
+		return ABasePawn::StaticClass();
+	}
+	struct Z_Construct_UClass_ABasePawn_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UECodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_ABasePawn_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_APawn,
+		(UObject* (*)())Z_Construct_UPackage__Script_ToonTanks,
+	};
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_ABasePawn_Statics::Class_MetaDataParams[] = {
+		{ "HideCategories", "Navigation" },
+		{ "IncludePath", "BasePawn.h" },
+		{ "ModuleRelativePath", "BasePawn.h" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_ABasePawn_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<ABasePawn>::IsAbstract,
+	};
+	const UECodeGen_Private::FClassParams Z_Construct_UClass_ABasePawn_Statics::ClassParams = {
+		&ABasePawn::StaticClass,
+		"Game",
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x009000A4u,
+		METADATA_PARAMS(Z_Construct_UClass_ABasePawn_Statics::Class_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UClass_ABasePawn_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_ABasePawn()
+	{
+		if (!Z_Registration_Info_UClass_ABasePawn.OuterSingleton)
+		{
+			UECodeGen_Private::ConstructUClass(Z_Registration_Info_UClass_ABasePawn.OuterSingleton, Z_Construct_UClass_ABasePawn_Statics::ClassParams);
+		}
+		return Z_Registration_Info_UClass_ABasePawn.OuterSingleton;
+	}
+	template<> TOONTANKS_API UClass* StaticClass<ABasePawn>()
+	{
+		return ABasePawn::StaticClass();
+	}
+	DEFINE_VTABLE_PTR_HELPER_CTOR(ABasePawn);
+	struct Z_CompiledInDeferFile_FID_ToonTanks_Source_ToonTanks_BasePawn_h_Statics
+	{
+		static const FClassRegisterCompiledInInfo ClassInfo[];
+	};
+	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_ToonTanks_Source_ToonTanks_BasePawn_h_Statics::ClassInfo[] = {
+		{ Z_Construct_UClass_ABasePawn, ABasePawn::StaticClass, TEXT("ABasePawn"), &Z_Registration_Info_UClass_ABasePawn, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(ABasePawn), 1095190564U) },
+	};
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_ToonTanks_Source_ToonTanks_BasePawn_h_2092217461(TEXT("/Script/ToonTanks"),
+		Z_CompiledInDeferFile_FID_ToonTanks_Source_ToonTanks_BasePawn_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_ToonTanks_Source_ToonTanks_BasePawn_h_Statics::ClassInfo),
+		nullptr, 0,
+		nullptr, 0);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
