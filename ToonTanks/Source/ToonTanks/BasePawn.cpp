e00800a49f84823dfbe43c7c7d2dc30a32837f13
Dimitris Chios <chiosdimit@gmail.com>
2023-07-09 20:03:12 +0300

    Created the basic component actor

--- ToonTanks/Source/ToonTanks/BasePawn.cpp
+++ ToonTanks/Source/ToonTanks/BasePawn.cpp
@@ -2,6 +2,7 @@
 
 
 #include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 ABasePawn::ABasePawn()
@@ -9,6 +10,8 @@ ABasePawn::ABasePawn()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	RootComponent = CapsuleComp;
 }
 
 // Called when the game starts or when spawned
--- ToonTanks/Source/ToonTanks/BasePawn.h
+++ ToonTanks/Source/ToonTanks/BasePawn.h
@@ -17,7 +17,11 @@ public:
 
 protected:
 	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	virtual void BeginPlay() override;\
+
+private:
+	UPROPERTY()
+	class UCapsuleComponent* CapsuleComp;
 
 public:	
 	// Called every frame
