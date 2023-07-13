225b326f1afda30e9fd657ae94c241cf0c36994f
Dimitris Chios <chiosdimit@gmail.com>
2023-07-13 17:50:22 +0300

    Created the GameMode Class
    Game Mode Class handles various games' features like Starting/Ending the game etc

--- ToonTanks/Intermediate/Build/Win64/UnrealEditor/Inc/ToonTanks/ToonTanks.init.gen.cpp
+++ ToonTanks/Intermediate/Build/Win64/UnrealEditor/Inc/ToonTanks/ToonTanks.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeToonTanks_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xB3D6A6E9,
-				0x6B5AA0EB,
+				0x8A85D477,
+				0x5B178AB8,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_ToonTanks.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_ToonTanks.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ToonTanks(Z_Construct_UPackage__Script_ToonTanks, TEXT("/Script/ToonTanks"), Z_Registration_Info_UPackage__Script_ToonTanks, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB3D6A6E9, 0x6B5AA0EB));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ToonTanks(Z_Construct_UPackage__Script_ToonTanks, TEXT("/Script/ToonTanks"), Z_Registration_Info_UPackage__Script_ToonTanks, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x8A85D477, 0x5B178AB8));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
