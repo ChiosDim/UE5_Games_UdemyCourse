b86bbac488943df80d028f5f8929749547d9d5e4
Dimitris Chios <chiosdimit@gmail.com>
2023-07-21 18:14:58 +0300

    Camera Rotation and Aim

--- SimpleShooter/Source/SimpleShooter/ShooterCharacter.h
+++ SimpleShooter/Source/SimpleShooter/ShooterCharacter.h
@@ -30,5 +30,12 @@ private:
 
 	void MoveForward(float AxisValue);
 	void MoveRight(float AxisValue);
+	void LookUpRate(float AxisValue);
+	void LookRightRate(float AxisValue);
+
+
+	UPROPERTY(EditAnywhere)
+	float RotationRate = 10;
+	
 
 };
