65fa79e9e8b6f4ec510ee2bb0ba4114b37f047d6
Dimitris Chios <chiosdimit@gmail.com>
2023-07-25 17:34:24 +0300

    Created the death animation

--- SimpleShooter/Source/SimpleShooter/ShooterCharacter.h
+++ SimpleShooter/Source/SimpleShooter/ShooterCharacter.h
@@ -21,6 +21,10 @@ protected:
 	virtual void BeginPlay() override;
 
 public:	
+
+	UFUNCTION(BlueprintCallable)
+	bool IsDead() const;
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
