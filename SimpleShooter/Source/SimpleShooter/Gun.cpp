f9c5387bb837d2800975d8362b97f78b2bffc138
Dimitris Chios <chiosdimit@gmail.com>
2023-07-25 11:33:12 +0300

    Created the Player View Point

--- SimpleShooter/Source/SimpleShooter/Gun.cpp
+++ SimpleShooter/Source/SimpleShooter/Gun.cpp
@@ -5,6 +5,7 @@
 
 #include "Components/SkeletalMeshComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values
 AGun::AGun()
@@ -23,6 +24,18 @@ AGun::AGun()
 void AGun::PullTrigger()
 {
 	UGameplayStatics::SpawnEmitterAttached(MuzzleFlash, Mesh, TEXT("MuzzleFlashSocket"));
+
+	APawn* OwnerPawn = Cast<APawn>(GetOwner());
+	if(OwnerPawn == nullptr) return;
+	AController* OwnerController = OwnerPawn->GetController();
+	if(OwnerController == nullptr) return; 
+
+	FVector Location;
+	FRotator Rotation;
+	OwnerController->GetPlayerViewPoint(Location, Rotation);
+
+	DrawDebugCamera(GetWorld(), Location, Rotation, 90, 2, FColor::Red, true);
+
 }
 
 // Called when the game starts or when spawned
