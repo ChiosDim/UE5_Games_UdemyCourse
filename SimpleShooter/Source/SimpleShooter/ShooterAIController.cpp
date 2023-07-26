1346f3fc1e9f303703e48cf8d78cab7bef919585
Dimitris Chios <chiosdimit@gmail.com>
2023-07-26 18:45:26 +0300

    Setting Blackboards keys in C++

--- SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
+++ SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
@@ -3,6 +3,7 @@
 
 #include "ShooterAIController.h"
 #include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 void AShooterAIController::BeginPlay()
 {
@@ -11,6 +12,9 @@ void AShooterAIController::BeginPlay()
     if(AIBehavior != nullptr)
     {
         RunBehaviorTree(AIBehavior);
+
+        APawn *PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+        GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
     }
    
 }
@@ -31,3 +35,4 @@ void AShooterAIController::Tick(float DeltaSeconds)
     //     StopMovement();
     // }
 }
+    
