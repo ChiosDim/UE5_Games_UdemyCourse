7d46bc38e87cd921bf4a8172d6255e24296faa0a
Dimitris Chios <chiosdimit@gmail.com>
2023-08-07 13:41:41 +0300

    fixed a bug with AI Shooting

--- SimpleShooter/Source/SimpleShooter/ShooterCharacter.cpp
+++ SimpleShooter/Source/SimpleShooter/ShooterCharacter.cpp
@@ -3,6 +3,7 @@
 
 #include "ShooterCharacter.h"
 #include "Gun.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 AShooterCharacter::AShooterCharacter()
@@ -59,6 +60,11 @@ float AShooterCharacter::TakeDamage(float DamageAmount, struct FDamageEvent cons
 	Health -= DamageToApply;
 	UE_LOG(LogTemp, Warning, TEXT("Health left %f"), Health);
 
+	if(IsDead())
+	{
+		DetachFromControllerPendingDestroy();
+		GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	}
 	return DamageToApply;
 }
 
