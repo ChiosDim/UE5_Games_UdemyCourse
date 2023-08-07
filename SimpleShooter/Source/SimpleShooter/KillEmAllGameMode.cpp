4145e0c2863b2c0871f1ec574c7d2b2c4ef69234
Dimitris Chios <chiosdimit@gmail.com>
2023-08-07 19:53:00 +0300

    Setting a restart timer

--- SimpleShooter/Source/SimpleShooter/KillEmAllGameMode.cpp
+++ SimpleShooter/Source/SimpleShooter/KillEmAllGameMode.cpp
@@ -8,5 +8,9 @@ void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
     Super::PawnKilled(PawnKilled);
 
-    UE_LOG(LogTemp, Warning, TEXT("A pawn was killed"));
+    APlayerController* PlayerController = Cast<APlayerController>(PawnKilled->GetController());
+    if(PlayerController != nullptr)
+    {
+        PlayerController->GameHasEnded(nullptr, false);
+    }
 }
--- SimpleShooter/Source/SimpleShooter/ShooterCharacter.cpp
+++ SimpleShooter/Source/SimpleShooter/ShooterCharacter.cpp
@@ -63,13 +63,16 @@ float AShooterCharacter::TakeDamage(float DamageAmount, struct FDamageEvent cons
 
 	if(IsDead())
 	{
-		DetachFromControllerPendingDestroy();
-		GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 		ASimpleShooterGameModeBase* GameMode = GetWorld()->GetAuthGameMode<ASimpleShooterGameModeBase>();
+
 		if(GameMode != nullptr)
 		{
 			GameMode->PawnKilled(this);
 		} 
+
+		DetachFromControllerPendingDestroy();
+		GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		
 	}
 	return DamageToApply;
 }
