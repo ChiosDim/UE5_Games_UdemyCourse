3a87263b40b0148a952347ce4f8735d207208703
Dimitris Chios <chiosdimit@gmail.com>
2023-08-07 18:58:37 +0300

    Created the Ending of the Game GameMode

--- SimpleShooter/Source/SimpleShooter/ShooterCharacter.cpp
+++ SimpleShooter/Source/SimpleShooter/ShooterCharacter.cpp
@@ -4,6 +4,7 @@
 #include "ShooterCharacter.h"
 #include "Gun.h"
 #include "Components/CapsuleComponent.h"
+#include "SimpleShooterGameModeBase.h"
 
 // Sets default values
 AShooterCharacter::AShooterCharacter()
@@ -64,6 +65,11 @@ float AShooterCharacter::TakeDamage(float DamageAmount, struct FDamageEvent cons
 	{
 		DetachFromControllerPendingDestroy();
 		GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		ASimpleShooterGameModeBase* GameMode = GetWorld()->GetAuthGameMode<ASimpleShooterGameModeBase>();
+		if(GameMode != nullptr)
+		{
+			GameMode->PawnKilled(this);
+		} 
 	}
 	return DamageToApply;
 }
